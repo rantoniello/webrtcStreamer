cf4dd6fb7050d014c6de95cfa2a98f01514b0909
Rafael Antoniello <rafael@ipstreaming.solutions>
2025-11-26 17:00:33 +0100

    Update main.cpp

--- src/draft/main.cpp
+++ src/draft/main.cpp
@@ -2,12 +2,14 @@
  */
 
 #include <nlohmann/json.hpp>
-#include "helpers.hpp"
-#include "dispatchqueue.hpp"
-#include "stream.hpp"
 #include <chrono>
 #include <thread>
+#include <mutex>
+#include <shared_mutex>
+#include <queue>
+#include <atomic>
 #include <sys/time.h>
+#include <rtc/rtc.hpp>
 
 #include <iostream>
 #include <fstream>
@@ -42,161 +44,12 @@ struct Frame {
 std::queue<Frame> framesFifo;
 thread vstreamThread;
 
-/// all connected clients
-unordered_map<string, shared_ptr<Client>> clients{};
-
-/// Creates peer connection and client representation
-/// @param config Configuration
-/// @param wws Websocket for signaling
-/// @param id Client ID
-/// @returns Client
-// Create and setup a PeerConnection
-class AVPeerManager {
-public:
-    AVPeerManager();
-    ~AVPeerManager();
-    void addRemotePeer(string id);
-    void setRemoteDescription(string description);
-private:
-    ///
-    std::shared_mutex _api_mutex;
-    ///
-    Configuration _conf;
-    /// Peers register
-    unordered_map<string, shared_ptr<PeerConnection>> _peersMap;
-};
-
-AVPeerManager::AVPeerManager() :
-        _api_mutex(),
-        _conf(),
-        _peersMap{}
-{
-    // Configuration: set STUN sevrer
-    string stunServer = "stun:stun.l.google.com:19302";
-    cout << "STUN server is " << stunServer << endl;
-    _conf.iceServers.emplace_back(stunServer);
-
-    // Configuration: disable auto-negotiation. If set to true, the user is
-    // responsible for calling 'rtcSetLocalDescription' after creating a data
-    // channel and after setting the remote description
-    _conf.disableAutoNegotiation = true;
-}
-
-AVPeerManager::~AVPeerManager()
-{
-}
-
-void AVPeerManager::addRemotePeer(string id)
-{
-    unique_lock lock(_api_mutex);
-}
-
-void AVPeerManager::setRemoteDescription(string description)
-{
-    unique_lock lock(_api_mutex);
-}
-
-shared_ptr<Client> createPeerConnection(const Configuration &config,
-                                                weak_ptr<WebSocket> wws,
-                                                string id) {
-    auto pc = make_shared<PeerConnection>(config);
-    auto client = make_shared<Client>(pc);
-
-    pc->onStateChange([id](PeerConnection::State state) {
-        cout << "State: " << state << endl;
-        if (state == PeerConnection::State::Disconnected ||
-            state == PeerConnection::State::Failed ||
-            state == PeerConnection::State::Closed) {
-            // remove disconnected client
-            MainThread.dispatch([id]() {
-                clients.erase(id);
-            });
-        }
-    });
-printf("**** %d\n", __LINE__); fflush(stdout); //FIXME!!
-    pc->onGatheringStateChange(
-        [wpc = make_weak_ptr(pc), id, wws](PeerConnection::GatheringState state) {
-        cout << "Gathering State: " << state << endl;
-        if (state == PeerConnection::GatheringState::Complete) {
-            if(auto pc = wpc.lock()) {
-                auto description = pc->localDescription();
-                json message = {
-                    {"id", id},
-                    {"type", description->typeString()},
-                    {"sdp", string(description.value())}
-                };
-                // Gathering complete, send answer
-                if (auto ws = wws.lock()) {
-                    ws->send(message.dump());
-                }
-            }
-        }
-    });
-
-    // Add video
-    {
-        const uint8_t payloadType = 102;
-        const uint32_t ssrc = 1;
-        const string cname = "video-stream";
-        const string msid = "stream1";
-        auto video = Description::Video(cname);
-        video.addH264Codec(payloadType);
-        video.addSSRC(ssrc, cname, msid, cname);
-        auto track = pc->addTrack(video);
-        // create RTP configuration
-        auto rtpConfig = make_shared<RtpPacketizationConfig>(ssrc, cname, payloadType, H264RtpPacketizer::ClockRate);
-        // create packetizer
-        auto packetizer = make_shared<H264RtpPacketizer>(NalUnit::Separator::StartSequence, rtpConfig);
-        // add RTCP SR handler
-        auto srReporter = make_shared<RtcpSrReporter>(rtpConfig);
-        packetizer->addToChain(srReporter);
-        // add RTCP NACK handler
-        auto nackResponder = make_shared<RtcpNackResponder>();
-        packetizer->addToChain(nackResponder);
-        // set handler
-        track->setMediaHandler(packetizer);
-        const function<void (void)> onOpen = [id, wc = make_weak_ptr(client)]()
-        {
-            auto c = wc.lock();
-            auto video = c->video.value();
-            vstreamThread = thread(vstreamThr, video); //FIXME!!
-            cout << "Video from " << id << " opened" << endl;
-        };
-        track->onOpen(onOpen);
-        auto trackData = make_shared<ClientTrackData>(track, srReporter);
-        client->video = trackData;
-    }
-
-    auto dc = pc->createDataChannel("ping-pong");
-    dc->onOpen([id, wdc = make_weak_ptr(dc)]() {
-        if (auto dc = wdc.lock()) {
-            dc->send("Ping");
-        }
-    });
-
-    dc->onMessage(nullptr, [id, wdc = make_weak_ptr(dc)](string msg) {
-        cout << "Message from " << id << " received: " << msg << endl;
-        if (auto dc = wdc.lock()) {
-            dc->send("Ping");
-        }
-    });
-    client->dataChannel = dc;
-
-    pc->setLocalDescription();
-    return client;
-};
-
-/// Main dispatch queue
-DispatchQueue MainThread("Main");
-
-/// Video stream
-optional<shared_ptr<Stream>> avStream = nullopt;
-
 const string defaultIPAddress = "127.0.0.1";
 const uint16_t defaultPort = 8000;
 string ip_address = defaultIPAddress;
 uint16_t port = defaultPort;
 
+#if 0
 void vstreamThr(shared_ptr<ClientTrackData> video)
 {
     /* Init encoder */
@@ -316,39 +169,197 @@ void wsOnMessage(json message, Configuration config, shared_ptr<WebSocket> ws) {
         }
     }
 }
+#endif
+
+/// Creates peer connection and client representation
+/// @param config Configuration
+/// @param wws Websocket for signaling
+/// @param id Client ID
+/// @returns Client
+// Create and setup a PeerConnection
+class WebRTCAVStreamer {
+public:
+    WebRTCAVStreamer();
+    ~WebRTCAVStreamer();
+    void addRemotePeer(string id);
+    void getRemotePeerDescription(string description);
+private:
+    ///
+    shared_mutex _api_mutex;
+    ///
+    Configuration _conf;
+    /// Peers register
+    unordered_map<string, shared_ptr<PeerConnection>> _peersMap;
+    /// Signaling web socket
+    shared_ptr<WebSocket> _signalingWS;
+    /// Signaling thread
+    thread _signalingThread;
+    ///
+    atomic_bool _do_term_signaling;
+};
+
+WebRTCAVStreamer::WebRTCAVStreamer() :
+        _api_mutex(),
+        _conf(),
+        _peersMap{},
+        _signalingWS(make_shared<WebSocket>()),
+        _do_term_signaling(false)
+{
+    // Configuration: set STUN sevrer
+    string stunServer = "stun:stun.l.google.com:19302";
+    cout << "STUN server is " << stunServer << endl;
+    _conf.iceServers.emplace_back(stunServer);
+
+    // Configuration: disable auto-negotiation. If set to true, the user is
+    // responsible for calling 'rtcSetLocalDescription' after creating a data
+    // channel and after setting the remote description
+    _conf.disableAutoNegotiation = true;
+
+    // Initialize signaling web socket
+    _signalingWS->onOpen([]() {
+        cout << "WebSocket connected, signaling ready" << endl;
+        //TODO: FIFO push {"signaling":"connected"}
+    });
+    _signalingWS->onClosed([&]() {
+        cout << "WebSocket closed" << endl;
+        _do_term_signaling = true;
+        //TODO: FIFO push {"signaling":"closed"}
+    });
+    _signalingWS->onError([](const string &error) {
+        cout << "WebSocket failed: " << error << endl; 
+    });
+    _signalingWS->onMessage([&](variant<binary, string> data) {
+        if (!holds_alternative<string>(data))
+            return;
+        json message = json::parse(get<string>(data));
+        //TODO: FIFO push {"signaling":"input message","body":<message>}
+        //wsOnMessage(message, config, ws); //FIXME!!: move to signaling thread
+    });
+    string localId = "server";
+    cout << "The local ID is: " << localId << endl;
+    const string url = "ws://" + ip_address + ":" + to_string(port) + "/" + localId; //FIXME!!: add in constructor
+    cout << "URL is " << url << endl;
+    _signalingWS->open(url);
+
+    //TODO: launch signaling thread
+    _signalingThread = thread([&] {
+            std::cout << "Launching signaling thread..." <<  "\n";
+
+            while (!_do_term_signaling) {
+                std::cout << "Signaling thread..." <<  "\n";
+                this_thread::sleep_for(std::chrono::seconds(1)); //FIXME!!
+
+                //TODO read FIFO messages and execute!
+            }
+    });
+}
+
+WebRTCAVStreamer::~WebRTCAVStreamer()
+{
+    _signalingWS->close();
+
+    //TODO: wait join signaling thread (thus confir it recieves close!! and unlock)
+    //TODO: join signaling thread (after stoping web socket)
+    _signalingThread.join();
+}
+
+void WebRTCAVStreamer::addRemotePeer(string peerId)
+{
+    unique_lock lock(_api_mutex);
+
+    // Allocate peer connection
+    auto pc = make_shared<PeerConnection>(_conf);
+
+    // Callbacks...
+    pc->onStateChange([peerId](PeerConnection::State state) {
+        cout << "State: " << state << endl;
+        if (state == PeerConnection::State::Disconnected ||
+            state == PeerConnection::State::Failed ||
+            state == PeerConnection::State::Closed) {
+            // remove disconnected client //TODO
+        }
+    });
+    pc->onGatheringStateChange([wpc = make_weak_ptr(pc), peerId]
+            (PeerConnection::GatheringState state) {
+        cout << "Gathering State: " << state << endl;
+        if (state == PeerConnection::GatheringState::Complete) {
+            if(auto pc = wpc.lock()) {
+                auto description = pc->localDescription();
+                json message = {
+                    {"id", peerId},
+                    {"type", description->typeString()},
+                    {"sdp", string(description.value())}
+                };
+                // Gathering complete, send answer
+                //if (auto ws = wws.lock()) {
+                //    ws->send(message.dump());
+                //}
+                //TODO: FIFO push {"signaling":"output message","body":<message>}
+            }
+        }
+    });
+
+    // Add video track to peer connection
+    const uint8_t payloadType = 102;
+    const uint32_t ssrc = 1;
+    const string cname = "video-stream";
+    const string msid = "stream1";
+    auto video = Description::Video(cname);
+    video.addH264Codec(payloadType);
+    video.addSSRC(ssrc, cname, msid, cname);
+    auto track = pc->addTrack(video);
+    // create RTP configuration
+    auto rtpConfig = make_shared<RtpPacketizationConfig>(ssrc, cname, 
+        payloadType, H264RtpPacketizer::ClockRate);
+    // create packetizer
+    auto packetizer = make_shared<H264RtpPacketizer>(
+        NalUnit::Separator::StartSequence, rtpConfig);
+    // add RTCP SR handler
+    auto srReporter = make_shared<RtcpSrReporter>(rtpConfig);
+    packetizer->addToChain(srReporter);
+    // add RTCP NACK handler
+    auto nackResponder = make_shared<RtcpNackResponder>();
+    packetizer->addToChain(nackResponder);
+    // set handler
+    track->setMediaHandler(packetizer);
+    const function<void (void)> onOpen = [peerId]() {
+        cout << "Video from " << peerId << " opened" << endl;
+        //TODO: FIFO push {"signaling":"video track opened","peerId":<id>}
+        //vstreamThread = thread(vstreamThr, video); //FIXME!!
+    };
+    track->onOpen(onOpen);
+
+    auto dc = pc->createDataChannel("ping-pong");
+    dc->onOpen([peerId, wdc = make_weak_ptr(dc)]() {
+        //TODO: FIFO push {"signaling":"data channel opened","peerId":<id>}
+        //if (auto dc = wdc.lock()) {dc->send("Ping");} //FIXME!!: action to do
+    });
+    dc->onMessage(nullptr, [peerId, wdc = make_weak_ptr(dc)](string msg) {
+        cout << "Message from " << peerId << " received: " << msg << endl;
+        //if (auto dc = wdc.lock()) {dc->send("Ping");} //FIXME!!: action to do
+    });
+
+    // Set local description
+    pc->setLocalDescription();
+
+    // Finally register peer connection in peers map.
+    this->_peersMap.emplace(peerId, make_weak_ptr(pc));
+}
+
+void WebRTCAVStreamer::getRemotePeerDescription(string description)
+{
+    unique_lock lock(_api_mutex);
+}
 
 int main(int argc, char **argv) try {
     int c = 0;
 
     InitLogger(LogLevel::Debug);
 
+    WebRTCAVStreamer streamer;
+
     string localId = "server";
     cout << "The local ID is: " << localId << endl;
-
-    auto ws = make_shared<WebSocket>();
-    ws->onOpen([]() { cout << "WebSocket connected, signaling ready" << endl; });
-    ws->onClosed([]() { cout << "WebSocket closed" << endl; });
-    ws->onError([](const string &error) { cout << "WebSocket failed: " << error << endl; });
-    ws->onMessage([&](variant<binary, string> data) {
-        if (!holds_alternative<string>(data))
-            return;
-        json message = json::parse(get<string>(data));
-//        MainThread.dispatch([message, config, ws]() {
-//            wsOnMessage(message, config, ws);
-//        });
-        wsOnMessage(message, config, ws);
-    });
-
-    const string url = "ws://" + ip_address + ":" + to_string(port) + "/" + localId;
-    cout << "URL is " << url << endl;
-    ws->open(url);
-
-    cout << "Waiting for signaling to be connected..." << endl;
-    while (!ws->isOpen()) {
-        if (ws->isClosed())
-            return 1;
-        this_thread::sleep_for(100ms);
-    }
 
     while (true) {
         string id;
@@ -366,3 +377,4 @@ int main(int argc, char **argv) try {
     std::cout << "Error: " << e.what() << std::endl;
     return -1;
 }
+
